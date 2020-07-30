f97390cda62b0c9c97a19120a4f37069181802e7
深秋 <3487467850@qq.com>
2020-07-30 17:25:55 +0800

    update clang

--- include/llvm/Support/VCSRevision.h
+++ include/llvm/Support/VCSRevision.h
@@ -1,2 +1,2 @@
-#define LLVM_REVISION "3678ad88a6a1cbc8f5171519666b10e8997289dc"
+#define LLVM_REVISION "73c12bd8ff1a9cd8375a357ea06f171e127ec1b8"
 #define LLVM_REPOSITORY "https://github.com/llvm/llvm-project"
